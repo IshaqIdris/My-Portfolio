c0a073f38580c245c48d4469955f50beea23587a
Ishaq Idris <idrismoha2@ecs.vuw.ac.nz>
2018-06-06 15:11:27 +1200

    transferred project

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fetch = require('node-fetch');
+
+const port = process.env.PORT || 5000;
+
+let projectsData = [
+    {
+        name: 'Shaqtech',
+        html_url: 'http://www.youtube.com/c/ishaqidris',
+        description: 'I review tech'
+    }
+];
+
+const githubRepoUrl = 'https://api.github.com/users/ishaqidris/repos';
+
+function getProjects () {
+    fetch(githubRepoUrl)
+        .then(res => res.json())
+        .then(projects => {
+
+            const gitHubProjects = projects.map(project => {
+                return {
+                    name: project.name,
+                    html_url: project.html_url,
+                    description: project.description
+                }
+            });
+
+            projectsData = projectsData.concat(gitHubProjects);
+            console.log(`Loaded ${projectsData.length} projects`);
+        });
+}
+
+getProjects();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api/projects', (req, res) => {
+    // Return projects as json
+    res.json(projectsData);
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+app.listen(port);
+console.log(`My Portfolio is listening on ${port}`);
